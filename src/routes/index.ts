fe2826211cf6565a659eb2d3ae5b80c3853e8331
Guilherme J. Floyd <gjpnkflyd@gmail.com>
2020-10-09 13:53:36 -0300

    DEPLOY #13

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,13 @@ import usersRouter from './users.routes';
 import totemsRouter from './totems.routes';
 import cartsRouter from './carts.routes';
 
+import { connectedTotems } from '../utils/sockets';
+
 const routes = Router();
+
+routes.get('/', (request, response) => {
+  return response.json({ totems: connectedTotems });
+});
 
 routes.use('/users', usersRouter);
 routes.use('/totems', totemsRouter);
